5ee12a29ba9e8ecb6a947d4e0851ce49ca199edf
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-03-27 01:29:22 +0900

    up

--- src/js/sketchMain.js
+++ src/js/sketchMain.js
@@ -13,27 +13,40 @@ const sketch = (p) => {
     p.createCanvas(p.windowWidth, p.windowHeight);
     p.smooth();
     p.background(255);
-    // 2本の交差した直線を描く
-    p.stroke(130, 0, 0);
-    p.strokeWeight(4);
-    p.line(
-      p.width / 2 - 70,
-      p.height / 2 - 70,
-      p.width / 2 + 70,
-      p.height / 2 + 70
-    );
-    p.line(
-      p.width / 2 + 70,
-      p.height / 2 - 70,
-      p.width / 2 - 70,
-      p.height / 2 + 70
-    );
-    // 円を描く
-    p.fill(255, 150);
-    p.ellipse(p.width / 2, p.height / 2, 50, 50);
+    p.frameRate(60);
+    p.noFill();
+    
+    _angnoise = p.random(10);
+    _radiusnoise = p.random(10);
+    _xnoise = p.random(10);
+    _ynoise = p.random(10);
+    
   };
   p.draw = () => {
     // put drawing code here
+    _radiusnoise += 0.005;
+    _radius = (p.noise(_radiusnoise) * 550) +1;
+    _angnoise += 0.005;
+    _angle += (p.noise(_angnoise) * 6) - 3;
+    if (_angle > 360) { _angle -= 360; }
+    if (_angle < 0) { _angle += 360; }
+    _xnoise += 0.01;
+    _ynoise += 0.01;
+    const centerx = p.width/2 + (p.noise(_xnoise) * 100) - 50;
+    const centery = p.height/2 + (p.noise(_ynoise) * 100) - 50;
+    const rad = p.radians(_angle);
+    const x1 = centerx + (_radius * p.cos(rad));
+    const y1 = centery + (_radius * p.sin(rad));
+    
+    const opprad = rad + p.PI;
+    const x2 = centerx + (_radius * p.cos(opprad));
+    const y2 = centery + (_radius * p.sin(opprad));
+    _strokeCol += _strokeChange;
+    if (_strokeCol > 254) { _strokeChange = -1; }
+    if (_strokeCol < 0) { _strokeChange = 1; }
+    p.stroke(_strokeCol, 60);
+    p.strokeWeight(1);
+    p.line(x1, y1, x2, y2);
   };
 };
 
