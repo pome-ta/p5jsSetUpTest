909ad88e4ae3c37fc4476c391c196dbacf54265f
pome-ta <pometarou93@gmail.com>
2023-03-26 14:49:14 +0900

    build min

--- src/js/p5Main.js
+++ src/js/p5Main.js
@@ -1,6 +1,3 @@
-import p5 from 'p5/lib/p5';
-// import * as s from 'p5/lib/addons/p5.sound.js';
-// const p5 = require('p5/lib/p5');
+import p5 from 'p5/lib/p5.min';
 
-// export { m as p5, s as p5Sound };
 export { p5 };
