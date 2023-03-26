1459476ff029c97c6efc953e25b5892a91f57f52
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-03-26 23:22:41 +0900

    up

--- src/js/sketchMain.js
+++ src/js/sketchMain.js
@@ -3,11 +3,12 @@ import { p5 } from './modules/p5Main.bundle.js';
 const sketch = (p) => {
   p.setup = () => {
     // 設定と背景
-    console.log(document.body);
+    //console.log(document.body);
     // p.createCanvas(document.body.clientWidth, document.body.clientHeight);
-    p.createCanvas(300, 400);
-    console.log(p.windowWidth);
-    console.log(document.body.clientWidth);
+    //p.createCanvas(300, 400);
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    //console.log(p.windowWidth);
+    //console.log(document.body.clientWidth);
     p.smooth();
     p.background(230, 230, 230);
     // 2本の交差した直線を描く
