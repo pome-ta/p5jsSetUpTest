c894e3208e544795e88204c0e2ac73e3df3d2058
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-03-29 00:08:15 +0900

    up

--- src/js/sketchBook/230328_2336.js
+++ src/js/sketchBook/230328_2336.js
@@ -3,33 +3,31 @@ import './modules/p5Sound.bundle.js';
 
 const sketch = (p) => {
   let cnvs;
-  let r, x, s;
+  let r, x;
 
+  function drawCircle() {
+    p.clear();
+    p.circle(x, p.height / 2, r * 2);
+  }
+
+  function addCounter() {
+    x += 10;
+    x = (x > p.width + r) ? -r : x;
+    drawCircle();
+  }
+
+  
   p.setup = () => {
     // put setup code here
     cnvs = p.createCanvas(p.windowWidth, p.windowHeight);
     r = p.min(p.width, p.height) / 6;
     x = r;
-    s = '0';
+    drawCircle();
+    cnvs.mouseClicked(addCounter);
   };
 
   p.draw = () => {
     // put drawing code here
-    x += 10;
-    x = (x > p.width + r) ? -r : x;
-    
-    p.clear();
-    p.circle(x, p.height / 2, r * 2);
-    
-    if (p.frameCount % 30 === 0) {
-      s = p.frameRate().toFixed(0);
-    }
-    
-    p.push();
-    p.noStroke();
-    p.fill(240);
-    p.text(s, 20, 20);
-    p.pop();
   };
 };
 
