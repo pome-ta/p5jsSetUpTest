927f9a57715345f59df7d437a79ab851952194da
pome-ta <pometarou93@gmail.com>
2023-03-28 21:23:05 +0900

    Update sketchMain.js

--- src/js/sketchMain.js
+++ src/js/sketchMain.js
@@ -1,33 +1,32 @@
 // import p5 from 'https://cdn.skypack.dev/p5';
 import './p5Setup.js';
-//import './modules/p5Sound.bundle.js';
+// import './modules/p5Sound.bundle.js';
 
 const sketch = (p) => {
   let w = p.windowWidth;
   let h = p.windowHeight;
   let r, x;
-  
-  
+
   function drawCircle() {
-    console.log(2)
+    console.log(2);
     p.clear();
     p.circle(x, p.height / 2, r * 2);
   }
-  
+
   p.setup = () => {
     // put setup code here
     p.createCanvas(w, h);
     r = p.min(p.width, p.height) / 6;
-    x = r
+    x = r;
+    drawCircle();
+    console.log(p);
+  };
+
+  p.mouseClicked = () => {
+    console.log(1);
+    x += 10;
     drawCircle();
   };
-  
-  p.mouseClicked = () => {
-    console.log(1)
-    x += 10;
-    drawCircle()
-    
-  }
   p.draw = () => {
     // put drawing code here
   };
