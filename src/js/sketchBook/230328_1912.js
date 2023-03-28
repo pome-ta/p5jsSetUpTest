53363434456512fae3abf6ca6f949391afd6ec50
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-03-28 20:51:16 +0900

    up

--- src/js/sketchBook/230328_1912.js
+++ src/js/sketchBook/230328_1912.js
@@ -5,32 +5,17 @@ import './p5Setup.js';
 const sketch = (p) => {
   let w = p.windowWidth;
   let h = p.windowHeight;
-  let r, x;
-  
-  
-  function drawCircle() {
-    console.log(2)
-    p.clear();
-    p.circle(x, p.height / 2, r * 2);
-  }
   
   p.setup = () => {
     // put setup code here
     p.createCanvas(w, h);
-    r = p.min(p.width, p.height) / 6;
-    x = r
-    drawCircle();
+    p.circle(p.width/2, p.height/2, 50);
+
   };
-  
-  p.mouseClicked = () => {
-    console.log(1)
-    x += 10;
-    drawCircle()
-    
-  }
   p.draw = () => {
     // put drawing code here
   };
 };
 
 const myp5 = new p5(sketch, 'p5Canvas');
+
