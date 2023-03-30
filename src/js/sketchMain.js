8ab8e11a6fd6320da25d8440c2f2081e30b819e2
pome-ta <pometarou93@gmail.com>
2023-03-30 23:24:08 +0900

    dev

--- src/js/sketchMain.js
+++ src/js/sketchMain.js
@@ -10,23 +10,22 @@ const sketch = (p) => {
     cnvs = p.createCanvas(p.windowWidth, p.windowHeight);
     w = p.width;
     h = p.height;
-    
+
     p.textAlign(p.CENTER, p.CENTER);
-    
+
     const outMargin = 24;
     x1 = outMargin;
     x2 = w - outMargin;
     x = x1;
     y = h / 2;
-    
   };
 
   p.draw = () => {
     // put drawing code here
     //x++;
-    x = (x > x2) ? x1 : x+1;
+    x = x > x2 ? x1 : x + 1;
     p.clear();
-    
+
     p.noStroke();
     p.fill(240);
     p.text(x1, x1, y - 65);
@@ -35,10 +34,7 @@ const sketch = (p) => {
     p.text(p.norm(x, x1, x2).toFixed(2), x, y + 65);
     p.text(0, x1, y + 65);
     p.text(1, x2, y + 65);
-    
-    
   };
 };
 
 const myp5 = new p5(sketch, 'p5Canvas');
-
