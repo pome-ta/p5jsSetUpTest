eda588db2c14054cd80849a9bc55ebe2ad315e97
pome-ta <pometarou93@gmail.com>
2023-03-31 23:59:29 +0900

    Update sketchMain.js

--- src/js/sketchMain.js
+++ src/js/sketchMain.js
@@ -3,7 +3,6 @@ import './p5Setup.js';
 
 const sketch = (p) => {
   let cnvs, w, h;
-  
 
   function reset() {
     w = p.width;
@@ -13,35 +12,32 @@ const sketch = (p) => {
 
     const yn = h / 8;
     const xn = w / 8;
-    
+
     for (let y = 0; y < yn; y++) {
       for (let x = 0; x < xn; x++) {
         const tx = (w / (xn - 1)) * x;
         const ty = (h / (yn - 1)) * y;
-        
+
         const dx = p.abs(tx - w / 2);
         const dy = p.abs(ty - h / 2);
-  
+
         const r = p.map(dx, 0, w / 2, 0, 255);
         const b = p.map(dy, 0, h / 2, 0, 255);
         p.fill(r, 0, b);
         p.circle(tx, ty, 10);
       }
     }
-    p.noLoop()
+    p.noLoop();
   }
 
   p.setup = () => {
     // put setup code here
     cnvs = p.createCanvas(p.windowWidth, p.windowHeight);
     reset();
-
-
   };
 
   p.draw = () => {
     // put drawing code here
-    
   };
 
   p.windowResized = () => {
@@ -51,4 +47,3 @@ const sketch = (p) => {
 };
 
 const myp5 = new p5(sketch, 'p5Canvas');
-
