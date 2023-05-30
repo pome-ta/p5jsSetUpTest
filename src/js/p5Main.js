d5f85827854f2c9e7b5078f10875ea3cb3756986
pome-ta <pometarou93@gmail.com>
2023-05-31 00:30:32 +0900

    up

--- src/js/p5Main.js
+++ src/js/p5Main.js
@@ -1,6 +1,6 @@
 // import p5 from 'p5/lib/p5.min';
-// import p5 from 'p5/lib/p5.min.js';
+import p5 from 'p5/lib/p5.min.js';
 // import p5 from 'p5/lib/p5';
-import p5 from 'p5/lib/p5.js';
+// import p5 from 'p5/lib/p5.js';
 
 export { p5 };
