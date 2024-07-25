82bcc9a6a578bc22a01311f4423ffe8539f40b5d
pome-ta <53405097+pome-ta@users.noreply.github.com>
2024-07-26 00:27:35 +0900

    analyze

--- src/js/sketchMain.js
+++ src/js/sketchMain.js
@@ -3,84 +3,88 @@ import './modules/p5Sound.bundle.js';
 
 const sketch = (p) => {
   let cnvs, w, h;
-  let t;
-  let osc;
-  let isPlaying = false;
-
-  const windowSizeUpDate = () => {
-  cnvs = p.createCanvas(p.windowWidth * 0.92, p.windowHeight*0.92);
+  let osc, fft;
   
-  }
   p.setup = () => {
     // put setup code here
-    windowSizeUpDate()
-    
-    t = 0;
+    windowSizeUpDate();
+    reset();
     
     osc = new p5.Oscillator('sine');
     osc.freq(440);
-    isPlaying = true;
     osc.start();
-    cnvs.mousePressed(p.play);
+    cnvs?.mousePressed(p.play);
+    
+    fft = new p5.FFT();
     
     
-    reset();
   };
-
-  const reset = () => {
-    w = p.width;
-    h = p.height;
-    p.background(0);
-    p.colorMode(p.HSB);
-
-    const yn = h / 16;
-    const xn = w / 16;
-    const maxDist = p.dist(0, 0, w / 2, h / 2);
-
-    for (let y = 0; y < yn; y++) {
-      for (let x = 0; x < xn; x++) {
-        const tx = (w / (xn - 1)) * x;
-        const ty = (h / (yn - 1)) * y;
-
-        const d = p.dist(tx, ty, w / 2, h / 2);
-        const hue = p.map(d, 0, maxDist, 0, 360);
-
-        p.fill(hue, 255, 255);
-        p.circle(tx, ty, 10);
-      }
-    }
-    //p.noLoop();
-  };
-
+  
   p.draw = () => {
     // put drawing code here
-    //console.log(p.millis());
-    t++;
-    //const sinVal = p.sin(t);
-    //osc.freq(osc.freq + sinVal * 10);
+    p.background(220);
+    
+    const spectrum = fft.analyze();
+    spectrum.forEach((v, i) => {
+      const x = p.map(i, 0, spectrum.length, 0, w);
+      const y = -h + p.map(v, 0, 255, h, 0);
+      p.rect(x, h, w / spectrum.length, y);
+    });
+    
+    
+    const waveform = fft.waveform();
+    p.noFill();
+    p.beginShape();
+    p.stroke(20);
+    waveform.forEach((v, i) => {
+      const x = p.map(i, 0, waveform.length, 0, w);
+      const y = p.map(v, -1, 1, 0, h);
+      p.vertex(x,y);
+    });
+    p.endShape();
+    
+    
   };
 
   p.play = () => {
-    isPlaying = true
-    //osc.start();
+    // todo: 発信してるか確認
     p.userStartAudio()
+    osc.freq(p.random(4400), 1);
   };
 
   p.windowResized = () => {
     windowSizeUpDate();
     reset();
   };
+
+  const windowSizeUpDate = () => {
+  cnvs = p.createCanvas(p.windowWidth * 0.92, p.windowHeight * 0.92);
+  //console.log(cnvs)
+  
+  }
+
+  const reset = () => {
+    w = p.width;
+    h = p.height;
+    
+    
+    //p.noLoop();
+  };
+
+
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  const myp5 = new p5(sketch, 'p5Canvas');
-  /*
-  document
-    .querySelector('#p5Canvas')
-    .addEventListener('touchmove', (e) => e.preventDefault(), {
-      passive: false,
-    });
-    */
+  const canvasId = 'p5Canvas';
+  //const myp5 = new p5(sketch, canvasId);
+  new p5(sketch, canvasId);
+  //console.log(myp5)
+  const canvasTag = document.querySelector(`#${canvasId}`);
+  canvasTag.addEventListener('touchmove', (e) => e.preventDefault(), { passive: false, });
+  //myp5.addEventListener('touchmove', (e) => e.preventDefault(), { passive: false, });
+  
+  //console.log(canvasTag)
+  
 });
 
 
