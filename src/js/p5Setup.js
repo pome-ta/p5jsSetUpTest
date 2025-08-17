4e70e25bcecf83d4c2850c3646c43c93e8b89907
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-08-17 11:00:13 +0900

    init

--- src/js/p5Setup.js
+++
@@ -1,4 +0,0 @@
-// import p5 from 'https://cdn.skypack.dev/p5';
-import { p5 } from './modules/p5Main.bundle.js';
-
-window.p5 = p5;
--- src/js/p5Sound.js
+++
@@ -1,2 +0,0 @@
-export * from 'p5/lib/addons/p5.sound.js';
-// export { default } from 'p5/lib/addons/p5.sound.js';
