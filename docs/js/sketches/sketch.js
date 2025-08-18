39ac3a6aebe040f2003e3348010fc4069147b3db
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-08-18 16:47:43 +0900

    sound 2.0

--- docs/js/sketches/sketch.js
+++ docs/js/sketches/sketch.js
@@ -1,27 +1,52 @@
-const sketch = (p) => {
+let osc, delay, env;
 
-  p.setup = () => {
-    // put setup code here
-    fitFullSizeCanvas();
-    p.background(255 - 23);
-  };
+function setup() {
+  let cnv = createCanvas(400, 400);
+  background(220);
+  textAlign(CENTER);
+  textSize(13);
+  text('click and drag mouse', width/2, 150);
 
-  p.draw = () => {
-    // put drawing code here
-  };
+  osc = new p5.Oscillator('sawtooth');
+  osc.amp(0.74);
+  env = new p5.Envelope(0.01);
+  delay = new p5.Delay(0.12, 0.7);
   
-  p.windowResized = (event) => {
-    fitFullSizeCanvas();
-  };
+  osc.disconnect();
+  osc.connect(env);
+  env.disconnect();
+  env.connect(delay);
 
-  function fitFullSizeCanvas() {
-    p.resizeCanvas(p.windowWidth, p.windowHeight);
-  }
+  cnv.mousePressed(oscStart);
+  cnv.mouseReleased(oscStop);
+  //cnv.mouseOut(oscStop);
+  describe('Click and release or hold, to play a square wave with delay effect.');
+}
 
+function oscStart() {
+  background(0, 255, 255);
+  text('release to hear delay', width/2, 150);
+  osc.start();
+  env.triggerAttack();
+}
+
+function oscStop() {
+  background(220);
+  text('click and drag mouse', width/2, 150);
+  env.triggerRelease();
+} 
+  
+function draw() {
+  osc.freq(map(mouseY, height, 0, 440, 880))
+  let dtime = map(mouseX, 0, width, 0.1, 0.5);
+  delay.delayTime(dtime);
+}
+
+function touchStarted(e) {
+    oscStart();
 };
 
-document.addEventListener('DOMContentLoaded', () => {
-  // --- start
-  const myp5 = new p5(sketch);
-  
-});
+function touchEnded(e) {
+    oscStop();
+};
+
